2e9adf4747fdf5162ef618705443e2f77232ad28
Evan Holmes <ivur.nave@gmail.com>
2019-10-06 16:46:06 -0500

    Updates

---
+++ precache-manifest.e4d612b67329003ae37504f50a6844bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa988f945406dee3575657c4457fd5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1f52bab9eba426be1a",
+    "url": "/static/css/main.8122b651.chunk.css"
+  },
+  {
+    "revision": "50d45ed319f4d6eb5826",
+    "url": "/static/js/2.fe4c9a1f.chunk.js"
+  },
+  {
+    "revision": "fc1f52bab9eba426be1a",
+    "url": "/static/js/main.792fb7c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b476af1384a537b37adbee9fff08fc59",
+    "url": "/static/media/cube.b476af13.png"
+  },
+  {
+    "revision": "d86ed72ba67b958a9d7a8a5cc64ef046",
+    "url": "/static/media/faces.d86ed72b.png"
+  },
+  {
+    "revision": "5e9e09af2d51235bfc6abeeee9438cb4",
+    "url": "/static/media/float-man.5e9e09af.png"
+  },
+  {
+    "revision": "2a7efe47e5025fe4827904f141a924cd",
+    "url": "/static/media/github.2a7efe47.svg"
+  },
+  {
+    "revision": "7eef2c83294b6f4a104ed7b3de6f07b7",
+    "url": "/static/media/itch.7eef2c83.svg"
+  },
+  {
+    "revision": "2dbad72989cd1882ea06f44460c9844a",
+    "url": "/static/media/linkedin.2dbad729.svg"
+  },
+  {
+    "revision": "8def92a727c583d9417c312921e4ce6e",
+    "url": "/static/media/mail.8def92a7.svg"
+  },
+  {
+    "revision": "6f2a9e6fd42f70caa672378463e59dd5",
+    "url": "/static/media/smile-sim.6f2a9e6f.png"
+  },
+  {
+    "revision": "e4885676ea853e4ea70ac7835be54ed5",
+    "url": "/static/media/this-is-big.e4885676.png"
+  },
+  {
+    "revision": "c38c586baffe73eb71465095a53599ee",
+    "url": "/static/media/wiggle.c38c586b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.e48110caedae06728358cdd442ad3e75.js"
+  "/precache-manifest.e4d612b67329003ae37504f50a6844bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
